378d03edc8447afa065410ce9a28cb78873e86bc
Ines Hwang <ihwang-24@peddie.org>
2022-04-04 22:41:53 -0400

    'yk

---
+++ app/src/main/java/ines/hwang/volunteen/Access.java
@@ -0,0 +1,21 @@
+package ines.hwang.volunteen;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Access extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.access);
+
+
+
+    }
+}
+
--- app/src/main/java/ines/hwang/volunteen/Feed.java
+++ app/src/main/java/ines/hwang/volunteen/Feed.java
@@ -1,6 +1,9 @@
 package ines.hwang.volunteen;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +13,18 @@ public class Feed extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.feed);
+        Button btn = findViewById(R.id.button14);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent it = new Intent(Feed.this, ServicePage.class);
+                startActivity(it);
+                finish();
+
+            }
+        });
     }
 }
 
